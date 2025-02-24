d8084b9144d4adff35da98b6cdb4bd6c27a5dd43
DashWisher <andrewryanonline@gmail.com>
2025-02-24 22:46:43 +0200

    new pages + created connections for new pages + added headers to new pages + added slideshow to free-time page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '..', 'views', 'main.html'));
-});
+router.get('/', (req, res) => {res.sendFile(path.join(__dirname, '..', 'views', 'main.html'));});
+
+router.get('/free_time', (req, res) => {res.sendFile(path.join(__dirname, '..', 'views', 'free_time.html'));});
+
+router.get('/work_experience', (req, res) => {res.sendFile(path.join(__dirname, '..', 'views', 'work_experience.html'));});
+
+router.get('/city', (req, res) =>{res.sendFile(path.join(__dirname, '..', 'views', 'city.html'));});
 
 module.exports = router;
